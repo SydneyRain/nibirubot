08d08984ddb44be0b177144fb55c2e9ca1fcca30
Sydney McKinley <syndeyrain6969@gmail.com>
2019-04-21 13:53:14 -0400

    Oops

--- commands/Games/Gambling/slots.js
+++ commands/Games/Gambling/slots.js
@@ -24,60 +24,60 @@ module.exports = class extends Command {
         let slot2 = slots[Math.floor(Math.random() * slots.length)];
         let slot3 = slots[Math.floor(Math.random() * slots.length)];
 
-        let checkIfWon = `${msg.author.username} bet  ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and lost all of it...`;
+        let checkIfWon = `${msg.author.username} bet  ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and lost all of it...`;
 
         if (slot1 == "🍇" && slot2 == "🍇" && slot3 == "🍇") {
-            checkIfWon = `${msg.author.username} bet  ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 35}**!`;
+            checkIfWon = `${msg.author.username} bet  ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 35}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 35 + parseInt(money));
         }
         
         if (slot1 == "🍈" && slot2 == "🍈" && slot3 == "🍈") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 35}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 35}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 35 + parseInt(money));
         }
         
         if (slot1 == "🍉" && slot2 == "🍉" && slot3 == "🍉") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 35}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 35}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 35 + parseInt(money));
         }
         
         if (slot1 == "🍊" && slot2 == "🍊" && slot3 == "🍊") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 60}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 60}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 60 + parseInt(money));
         }
                 
         if (slot1 == "🍋" && slot2 == "🍋" && slot3 == "🍋") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 60}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 60}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 60 + parseInt(money));
         }
                         
         if (slot1 == "🍌" && slot2 == "🍌" && slot3 == "🍌") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 40}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 40}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 40 + parseInt(money));
         }
            
         if (slot1 == "🍑" && slot2 == "🍑" && slot3 == "🍑") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 37}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 37}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 37 + parseInt(money));
         }
                    
         if (slot1 == "🍅" && slot2 == "🍅" && slot3 == "🍅") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 45}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 45}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 45 + parseInt(money));
         }
                            
         if (slot1 == "🍓" && slot2 == "🍓" && slot3 == "🍓") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 55}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 55}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 55 + parseInt(money));
         }
                                    
         if (slot1 == "💩" && slot2 == "💩" && slot3 == "💩") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) + 5}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) + 5}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) + 5 + parseInt(money));
         }
                                    
         if (slot1 == ":seven:" && slot2 == ":seven:" && slot3 == ":seven:") {
-            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencysymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencysymbol}**${parseInt(money) * 300}**!`;
+            checkIfWon = `${msg.author.username} bet ${msg.guild.settings.currencySymbol}**${parseInt(money)}**, and won ${msg.guild.settings.currencySymbol}**${parseInt(money) * 300}**!`;
             await msg.author.settings.update("money", parseInt(msg.author.settings.money) + parseInt(money) * 300 + parseInt(money));
         }
         
